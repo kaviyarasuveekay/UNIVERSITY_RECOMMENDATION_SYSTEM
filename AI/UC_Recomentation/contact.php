d80a97df3858c0bf5b60c68a74cd6111a04397b0
Kaviyarasu V <84955527+kaviyarasuveekay@users.noreply.github.com>
2022-07-30 21:22:58 +0530

    Add files via upload

---
+++ AI/UC_Recomentation/contact.php
@@ -0,0 +1,101 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Contact us</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+					<a href="index.html" style="font-size: 30px;">AI PROJECT</a>
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+
+						<li class="active"><a href="contact.php">contact</a></li>
+					</ul>
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">
+				<div class="col span_1_of_3">
+
+      			<div class="company_address">
+				     	<h2>Our Address  :</h2>
+						    	<p>7A/2 AKVS street,</p>
+						   		<p>opposite to VIT vellore,</p>
+						   		<p>vellore-639118</p>
+				   		<p>Reception No:04324567894</p>
+				   		<h2>Head Office:</h2>
+				   		<p>Email: <span>headoffice@gmail.com</span></p>
+				   		<p>Mobile: 7689795087</p>
+				   		<h2>Complaints:</h2>
+				 	 	<p>Email: <span>complaints@gmail.com</span></p>
+				 	 	<p>Mobile: 9791453756</p>
+
+				   </div>
+				</div>
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contact Us</h2>
+					    <form>
+					    	<div>
+						    	<span><label>NAME</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>MOBILE.NO</label></span>
+						    	<span><input type="text" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>SUBJECT</label></span>
+						    	<span><textarea> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" value="Submit"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      <div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.html">Home</a></li>
+
+						<li><a href="contact.php">contact</a></li>
+					</ul>
+		   	</div>
+
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+	</body>
+</html>
